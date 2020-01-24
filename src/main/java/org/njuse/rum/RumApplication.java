f0da98d9c445df8906d1c9534fdf1dff74077c96
Gryffindor <838492657@qq.com>
2020-01-24 16:52:40 +0800

    搭建项目结构

---
+++ src/main/java/org/njuse/rum/RumApplication.java
@@ -0,0 +1,13 @@
+package org.njuse.rum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/njuse/rum/RumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.njuse.rum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
